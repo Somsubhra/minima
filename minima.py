e59ef90e8fdd6ad996256608d9f647cbc5727430
Somsubhra Bairi <somsubhra.b@endurance.com>
2017-01-03 13:35:17 +0530

    Get all the instances on a hypervisor

--- minima.py
+++ minima.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
-from minima import HyperVisor
+from minima import HyperVisor, Instance
 
-hyperVisor = HyperVisor()
-hyperVisor.print_host_info()
+hyper_visor = HyperVisor()
+hyper_visor.print_host_info()
+
+instance = Instance(hyper_visor)
+instance.print_all_domains()
--- minima/instance.py
+++ minima/instance.py
@@ -1,4 +1,17 @@
 class Instance:
 
-    def __init__(self):
-        pass
+    def __init__(self, hyper_visor):
+        self.hyper_visor = hyper_visor
+        self.conn = hyper_visor.get_connection()
+
+    def print_all_domains(self):
+        domains = self.get_all_domains()
+
+        if len(domains) != 0:
+            for domain in domains:
+                print domain.name()
+        else:
+            print "No instances found on hypervisor " + self.hyper_visor.get_host_name()
+
+    def get_all_domains(self):
+        return self.conn.listAllDomains(0)
